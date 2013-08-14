6eb67e33b059f553a1d98036032ee536b40e7795
Scott Murray <scott.murray@eegeo.com>
2013-08-14 09:18:22 +0100

    Fix https://bugs.eegeo.com/browse/MPLY-2614 - this is a defect in android native glue handling of software keyboard - the keyboard is a different activity so needs to be handled as described in this SO post http://stackoverflow.com/questions/15913080/crash-when-closing-soft-keyboard-while-using-native-activity - note that this just allows the app to intercept the close event, this fix handles it by blocking the closing of the keyboard by not responding to the event, because its a keyboard sample.

--- jni/main.cpp
+++ jni/main.cpp
@@ -2,6 +2,25 @@
 #include <android_native_app_glue.h>
 #include "AppWindow.hpp"
 
+static void process_input( struct android_app* state, struct android_poll_source* source)
+{
+    AInputEvent* event = NULL;
+    if (AInputQueue_getEvent(state->inputQueue, &event) >= 0) {
+        int type = AInputEvent_getType(event);
+        bool skip_predispatch
+              =  AInputEvent_getType(event)  == AINPUT_EVENT_TYPE_KEY
+              && AKeyEvent_getKeyCode(event) == AKEYCODE_BACK;
+
+        if (!skip_predispatch && AInputQueue_preDispatchEvent(state->inputQueue, event)) {
+            return;
+        }
+
+        int32_t handled = 0;
+        if (state) handled = state->onInputEvent(state, event);
+        AInputQueue_finishEvent(state->inputQueue, event, handled);
+    }
+}
+
 static int32_t engine_handle_input(struct android_app* state, AInputEvent* event)
 {
 	AppWindow* app = (AppWindow*)state->userData;
@@ -18,6 +37,7 @@ void android_main(struct android_app* state)
 {
     app_dummy();
 	AppWindow window(state);
+    state->inputPollSource.process = process_input;
     state->onAppCmd = engine_handle_cmd;
     state->onInputEvent = engine_handle_input;
     state->userData = &window;
