11825d1ed38c1ea9fff03ea51c91fc791e42c854
Scott Murray <scott.murray@eegeo.com>
2013-09-10 11:04:52 +0100

    Echo platform version on when unpacking platform libs.

--- update.platform.sh
+++ update.platform.sh
@@ -4,4 +4,6 @@ rm -rf ./libs
 curl http://s3.amazonaws.com/eegeo-static/sdk.package.android.tar.gz > ./sdk.package.tar.gz
 tar -zxvf ./sdk.package.tar.gz
 rm -f ./sdk.package.tar.gz
+value=`cat ./sdk.package.android/version.txt`
+echo "Platform version --> $value"
 mv ./sdk.package.android/ ./libs
