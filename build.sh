833e50191d21687a9837d01fa7202a29eec838c4
Mark Simpson <mark.simpson@eegeo.com>
2013-10-14 10:46:58 +0100

    Branch on OS for build script to make it work on windows. Buddy: None (nobody available)

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ rm -rf "./bin"
 
 sh "update.platform.sh"
 
-ndk-build
+# running on msys (basically the git provided shell we use on windows)
+if [ "$OSTYPE" == "msys" ]; then
+    cmd "/C ndk-build.cmd"
+else
+    ndk-build
+fi
+
 resultcode=$?
 
 echo
