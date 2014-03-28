4687bdee3314f5cf013a3098936f94efc0db3ac9
Mark Simpson <mark.simpson@eegeo.com>
2014-03-28 10:55:18 +0000

    Added some comments on the process of transforming a point from ecef space through to screen space. Buddy: Salva

--- jni/Examples/PositionJavaPinButtonExample.cpp
+++ jni/Examples/PositionJavaPinButtonExample.cpp
@@ -121,7 +121,6 @@ namespace Examples
     void PositionJavaPinButtonExample::Project (const Eegeo::Space::LatLongAltitude& location, Eegeo::v3& screenPosition)
 	{
     	//project a 3D Ecef location to the screen
-		float viewport[] = {0, 0, m_renderContext.GetScreenWidth(), m_renderContext.GetScreenHeight()};
 		Eegeo::m44 finalMatrix;
 
 		Eegeo::m44::Mul (finalMatrix,
@@ -131,15 +130,21 @@ namespace Examples
 		Eegeo::v3 local = (location.ToECEF() - m_renderContext.GetCameraOriginEcef()).ToSingle();
 		Eegeo::v4 inVector(local, 1.0f);
 
+		// get clip space coords
 		Eegeo::v4 outVector = Eegeo::v4::Mul(inVector, finalMatrix);
 
+		// divide through by w to get normalized device space coords -- range [-1, 1]
 		screenPosition.SetX((outVector.GetX()/outVector.GetW()));
 		screenPosition.SetY((outVector.GetY()/outVector.GetW()));
 		screenPosition.SetZ((outVector.GetZ()/outVector.GetW()));
 
+		// transform from [-1, 1] to [0, 1]
 		screenPosition.SetX((screenPosition.GetX() + 1.0f) * 0.5f);
 		screenPosition.SetY(1.0f - ((screenPosition.GetY() + 1.0f) * 0.5f));
 
+		float viewport[] = {0, 0, m_renderContext.GetScreenWidth(), m_renderContext.GetScreenHeight()};
+
+		// transform from [0, 1] to screen coords.
 		screenPosition.SetX((screenPosition.GetX()*(viewport[2]-viewport[0])) + viewport[0]);
 		screenPosition.SetY((screenPosition.GetY()*(viewport[3]-viewport[1])) + viewport[1]);
 	}
